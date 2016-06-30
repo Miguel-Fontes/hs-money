1f2bc9568246c435d710defa2bc9d0304c0cf48d
Miguel Fontes <miguel_m_fontes@hotmail.com>
2016-06-30 15:36:40 -0300

    Formatação de código

--- src/Persist/FileDB.hs
+++ src/Persist/FileDB.hs
@@ -8,29 +8,29 @@ data FileDB = FileDB String
 getName (FileDB name) = name
 
 instance Database FileDB where
-    queryDB = query'
-    getDB = get'
-    saveDB = save'
-    updateDB = update'
-    deleteDB = delete'
+    queryDB  = query
+    getDB    = get
+    saveDB   = save
+    updateDB = update
+    deleteDB = delete
 
 connect :: String -> FileDB
 connect nome = FileDB nome
 
-query' :: (Serializable a) => FileDB -> a -> IO a
-query' db _ = do
+query :: (Serializable a) => FileDB -> a -> IO a
+query db _ = do
     db <- readFile (getName db)
     return (parse db)
 
-get' :: (Serializable a) => FileDB -> a -> IO a
-get' _ = undefined
+get :: (Serializable a) => FileDB -> a -> IO a
+get _ = undefined
 
-save' :: (Serializable a) => FileDB -> a -> IO()
-save' db = writeFile (getName db) . serialize
+save :: (Serializable a) => FileDB -> a -> IO()
+save db = writeFile (getName db) . serialize
 
-update' :: (Serializable a) => FileDB -> a -> IO()
-update' _ = undefined
+update :: (Serializable a) => FileDB -> a -> IO()
+update _ = undefined
 
-delete' :: (Serializable a) => FileDB -> a -> IO()
-delete' _ = undefined
+delete :: (Serializable a) => FileDB -> a -> IO()
+delete _ = undefined
 
--- src/Persist/Persistable.hs
+++ src/Persist/Persistable.hs
@@ -4,17 +4,17 @@ import Persist.Serializable
 import Persist.Database
 
 class (Serializable a) => Persistable a where
-    query  :: (Database d) => d -> a -> IO a
+    query  :: (Database d) => d -> (a -> IO a)
     query  = queryDB
 
-    save   :: (Database d) => d -> a -> IO()
+    save   :: (Database d) => d -> (a -> IO())
     save   = saveDB
 
-    update :: (Database d) => d -> a -> IO()
+    update :: (Database d) => d -> (a -> IO())
     update = updateDB
 
-    delete :: (Database d) => d -> a -> IO()
+    delete :: (Database d) => d -> (a -> IO())
     delete = deleteDB
 
-    get    :: (Database d) => d -> a -> IO a
+    get    :: (Database d) => d -> (a -> IO a)
     get    = getDB
